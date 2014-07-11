b62f0b47f924f06f9c1292455a0d257f52617457
Mike-Witt <msg2mw@gmail.com>
2014-07-11 16:17:16 -0700

    checkin from Backie

--- SG/mknblst.py
+++ SG/mknblst.py
@@ -6,10 +6,10 @@ import glob
 
 def mklist1():
     for nb in glob.glob('*.ipynb'):
-        str = '<li><a href="'
+        str = '<li><b><a href="'
         str += 'http://nbviewer.ipython.org/url/mike-witt.github.io/SG/'
-        str += nb + '">' + nb
-        str += '<a href="' + nb + '">Download</a>'
+        str += nb + '"> | ' + nb
+        str += '<a href="' + nb + '">Download</a></b>'
         str += '\n<p>'
         print str
 mklist1()
