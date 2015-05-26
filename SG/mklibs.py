897b3e07375e40cb7e99634047e726b375478327
Mike-Witt <msg2mw@gmail.com>
2015-05-25 19:14:45 -0700

    checkin from Vector

---
+++ SG/mklibs.py
@@ -0,0 +1,18 @@
+#
+# Make an html list of libraries
+#
+
+import glob
+
+def mklibs():
+    libs = glob.glob('sglib*.py')
+    libs.sort()
+    for nb in libs:
+        str = '<li><b>' + nb + '</b>'
+        str += ' | <a href="' + nb + '">Download</a>'
+        str += '\n<p>'
+        print(str)
+    print("</ul>")
+    print('<pre>\n\n</pre>')
+
+mklibs()
